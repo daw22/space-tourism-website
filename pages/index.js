102c60f550eae845206a11f335e95d61fd92c7b1
daw22 <dawitxxd@gmail.com>
2021-12-28 20:14:17 -1200

    second commit

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
                 <Title>so, you want to travle to</Title>
                 <MainTitle>SPACE</MainTitle>
                 <Description>
-                  Let's face it; if you want to go to space, you might as well genuinely go to outer space and not 
+                  Let{"'"}s face it; if you want to go to space, you might as well genuinely go to outer space and not 
                   hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of 
                   this world experience!
                 </Description>
